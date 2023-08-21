af68c3a0306e5cb013ee3d0bf7f3c7652b386cad
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-08-21 11:13:13 +0300

    создал простую визуальную демонстрацию

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,28 @@
 const controller = new InputController();
 
-controller.onInput('w', () => {
-    console.log('Pressed W!');
-})
+controller.onInput('KeyW', () => {
+    const arrow = document.querySelector('img#arrowMain');
+
+    arrow.classList.remove('left', 'bottom', 'right');
+});
+
+controller.onInput('KeyA', () => {
+    const arrow = document.querySelector('img#arrowMain');
+    
+    arrow.classList.remove('left', 'bottom', 'right');
+    arrow.classList.add('left');
+});
+
+controller.onInput('KeyS', () => {
+    const arrow = document.querySelector('img#arrowMain');
+    
+    arrow.classList.remove('left', 'bottom', 'right');
+    arrow.classList.add('bottom');
+});
+
+controller.onInput('KeyD', () => {
+    const arrow = document.querySelector('img#arrowMain');
+    
+    arrow.classList.remove('left', 'bottom', 'right');
+    arrow.classList.add('right');
+});
