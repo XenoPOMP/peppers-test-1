3dacb844c79a89f25be25b63a46f1af08f77df04
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-08-21 12:51:26 +0300

    удалил ненужные строки из тестов

--- src/js/input-controller-test.js
+++ src/js/input-controller-test.js
@@ -51,29 +51,6 @@ consoleGroup('Проверка метода disableAction', () => {
     controller.enableAction('left');
 });
 
-// controller.onInput('KeyW', () => {
-//     const arrow = document.querySelector('img#arrowMain');
-
-//     arrow.classList.remove('left', 'bottom', 'right');
-// });
-
-// controller.onInput('KeyA', () => {
-//     const arrow = document.querySelector('img#arrowMain');
-    
-//     arrow.classList.remove('left', 'bottom', 'right');
-//     arrow.classList.add('left');
-// });
-
-// controller.onInput('KeyS', () => {
-//     const arrow = document.querySelector('img#arrowMain');
-    
-//     arrow.classList.remove('left', 'bottom', 'right');
-//     arrow.classList.add('bottom');
-// });
-
-// controller.onInput('KeyD', () => {
-//     const arrow = document.querySelector('img#arrowMain');
-    
-//     arrow.classList.remove('left', 'bottom', 'right');
-//     arrow.classList.add('right');
-// });
+consoleGroup('Проверка метода attach', () => {
+    controller.attach(document);
+});
