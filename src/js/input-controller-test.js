8975f0df6785793add2c6afdba3d508fd10c4a37
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-08-21 15:42:02 +0300

    создал вывод данных на страницу

--- src/js/input-controller-test.js
+++ src/js/input-controller-test.js
@@ -15,51 +15,78 @@ const controller = new InputController({
     }
 });
 
-const stopEventListening = () => {
-    controller.detach();
+controller.attach(document, true);
+
+/** Выводим данные. */
+const inlineData = () => {
+    /** @type {string} */
+    const textToInsert = [
+        `Контроллер включен: ${controller.enabled}`,
+        '',
+        'Actions:', 
+        JSON.stringify(controller.actions, null, 2)
+    ].join('\n');
+
+    document.querySelector('#info-preview p').innerText = textToInsert;
 }
 
-consoleGroup('Проверка метода bindActions', () => {
-    console.log(controller.actions);
+inlineData();
 
-    controller.bindActions({
-        right: {
-            keys: [100],
-            enabled: false
-        }
-    });
+/** @type {Record<string, HTMLButtonElement|null>} */
+const devButtons = {
+    attachButton: document.querySelector('button#attach-test-button'),
+    detachButton: document.querySelector('button#detach-test-button')
+};
 
-    console.log(controller.actions);
-});
+devButtons.attachButton.onclick = () => {
+    controller.attach(document.body);
+};
 
-consoleGroup('Проверка метода isActionActive', () => {
-    console.log(`Action[bottom] активно: %c${controller.isActionActive('bottom')}`, 'color: blue;');
-    console.log(`Action[left] активно: %c${controller.isActionActive('left')}`, 'color: blue;');
-    console.log(`Action[right] активно: %c${controller.isActionActive('right')}`, 'color: blue;');
-});
-
-consoleGroup('Проверка метода enableAction', () => {
-    controller.enableAction('topLeft');
-    controller.enableAction('right');
-
-    console.log(`Action[right] активно: %c${controller.isActionActive('right')}`, 'color: blue;');
-});
-
-consoleGroup('Проверка метода disableAction', () => {
-    controller.disableAction('topLeft');
-    controller.disableAction('left');
-
-    console.log(`Action[left] активно: %c${controller.isActionActive('left')}`, 'color: blue;');
-
-    controller.enableAction('right');
-    controller.enableAction('left');
-});
-
-consoleGroup('Проверка метода attach', () => {
-    controller.attach(document);
-});
-
-consoleGroup('Проверка метода detach', () => {
+devButtons.detachButton.onclick = () => {
     controller.detach();
-    // controller.attach(document);
-});
+};
+
+// consoleGroup('Проверка метода bindActions', () => {
+//     console.log(controller.actions);
+
+//     controller.bindActions({
+//         right: {
+//             keys: [100],
+//             enabled: false
+//         }
+//     });
+
+//     console.log(controller.actions);
+// });
+
+// consoleGroup('Проверка метода isActionActive', () => {
+//     console.log(`Action[bottom] активно: %c${controller.isActionActive('bottom')}`, 'color: blue;');
+//     console.log(`Action[left] активно: %c${controller.isActionActive('left')}`, 'color: blue;');
+//     console.log(`Action[right] активно: %c${controller.isActionActive('right')}`, 'color: blue;');
+// });
+
+// consoleGroup('Проверка метода enableAction', () => {
+//     controller.enableAction('topLeft');
+//     controller.enableAction('right');
+
+//     console.log(`Action[right] активно: %c${controller.isActionActive('right')}`, 'color: blue;');
+// });
+
+// consoleGroup('Проверка метода disableAction', () => {
+//     controller.disableAction('topLeft');
+//     controller.disableAction('left');
+
+//     console.log(`Action[left] активно: %c${controller.isActionActive('left')}`, 'color: blue;');
+
+//     controller.enableAction('right');
+//     controller.enableAction('left');
+// });
+
+// consoleGroup('Проверка метода attach', () => {
+//     controller.attach(document);
+// });
+
+// consoleGroup('Проверка метода detach', () => {
+//     controller.detach();
+//     // controller.attach(document);
+// });
