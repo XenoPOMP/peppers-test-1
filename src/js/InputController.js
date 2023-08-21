606ac2d502e99ed1a64b7c33087634dc3fde4bac
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-08-21 11:35:49 +0300

    создал метод для биндинга экшенов

--- src/js/InputController.js
+++ src/js/InputController.js
@@ -3,7 +3,53 @@
  * других контроллеров.
  */
 class InputController {
-    constructor() {}
+    /** @type {boolean} */
+    enabled = true;
+    /** @type {boolean} */
+    focused = true;
+
+    /** @type {string|undefined} */
+    ACTION_ACTIVATED = undefined;
+    /** @type {string|undefined} */
+    ACTION_DEACTIVATED = undefined;
+
+    /** @type {Record<string, { keys: number[], enabled?: boolean }>} */
+    actions = {};
+
+    /** @type {HTMLElement|Document} */
+    target = document;
+
+    /**
+     * @param {Record<string, { keys: number[], enabled?: boolean }>} actionsToBind 
+     * @param {HTMLElement} [target] 
+     */
+    constructor(actionsToBind, target) {
+        /** 
+         * Присваиваем переменной target значение из аргумента,
+         * есди таковое имеется.
+         */
+        if (target !== undefined) {
+            this.target = target;
+        }
+
+        /** Если в конструктор были переданы события, то биндим их. */
+        if (actionsToBind !== undefined) {
+            this.bindActions(actionsToBind);
+        }
+    }
+
+    /**
+     * Этот метод занимается биндингом событий.
+     * 
+     * @param {Record<string, { keys: number[], enabled?: boolean }>} actionsToBind 
+     */
+    bindActions(actionsToBind) {
+        /** 
+         * Разворачиваем объект с новыми экшенами
+         * в переменную actions.
+         */
+        this.actions = { ...this.actions, ...actionsToBind };
+    }
 
     /**
      * Метод, обрабатывающий событие, возникающее при
@@ -13,8 +59,8 @@ class InputController {
      * @param {() => any} action            колбэк, вызываемый в момент нажатия. 
      */
     onInput(key, action) {
-        /** Вешаем на документ обработчик событий keypress. */
-        document.addEventListener('keypress', (ev) => {
+        /** Вешаем на цель обработчик событий keypress. */
+        this.target.addEventListener('keypress', (ev) => {
             /** 
              * Если кнопка из события совпадает с целевой,
              * выполняем колбэк.
--- src/js/script.js
+++ src/js/script.js
@@ -1,28 +1,44 @@
-const controller = new InputController();
-
-controller.onInput('KeyW', () => {
-    const arrow = document.querySelector('img#arrowMain');
-
-    arrow.classList.remove('left', 'bottom', 'right');
+const controller = new InputController({
+    left: {
+        keys: [],
+        enabled: true
+    }
 });
 
-controller.onInput('KeyA', () => {
-    const arrow = document.querySelector('img#arrowMain');
+console.log(controller.actions);
+
+controller.bindActions({
+    right: {
+        keys: [],
+        enabled: false
+    }
+});
+
+console.log(controller.actions);
+
+// controller.onInput('KeyW', () => {
+//     const arrow = document.querySelector('img#arrowMain');
+
+//     arrow.classList.remove('left', 'bottom', 'right');
+// });
+
+// controller.onInput('KeyA', () => {
+//     const arrow = document.querySelector('img#arrowMain');
     
-    arrow.classList.remove('left', 'bottom', 'right');
-    arrow.classList.add('left');
-});
+//     arrow.classList.remove('left', 'bottom', 'right');
+//     arrow.classList.add('left');
+// });
 
-controller.onInput('KeyS', () => {
-    const arrow = document.querySelector('img#arrowMain');
+// controller.onInput('KeyS', () => {
+//     const arrow = document.querySelector('img#arrowMain');
     
-    arrow.classList.remove('left', 'bottom', 'right');
-    arrow.classList.add('bottom');
-});
+//     arrow.classList.remove('left', 'bottom', 'right');
+//     arrow.classList.add('bottom');
+// });
 
-controller.onInput('KeyD', () => {
-    const arrow = document.querySelector('img#arrowMain');
+// controller.onInput('KeyD', () => {
+//     const arrow = document.querySelector('img#arrowMain');
     
-    arrow.classList.remove('left', 'bottom', 'right');
-    arrow.classList.add('right');
-});
+//     arrow.classList.remove('left', 'bottom', 'right');
+//     arrow.classList.add('right');
+// });
